774263da41e35f0dc3f3f026265d7f330c9d44bf
Henyers Baltar <henyers.b24@gmail.com>
2021-01-31 09:59:09 -0600

    removed defected code

--- Music_Room/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path #forgot .urls
-from . import views
-
-urlpatterns = [ 
-    path('create_room', views.RoomCreate.as_view()), #as_view()
-    path('', views.RoomView.as_view()),
-    path('get-room', views.GetRoom.as_view()),
-
-]
--- Music_Room/frontend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index),
-    path('join', views.index),
-    path('create', views.index),
-    path('room/<str:roomCode>', views.index),
-]
